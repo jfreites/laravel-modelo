9e8b95be74755f0aed3ff413112fa2684eba26b8
Carlos Castillo <carlos@koalabs.co>
2013-02-28 23:16:51 -0500

    Tail. Javascript files at the end of the body

--- config/modelo.php
+++ config/modelo.php
@@ -46,4 +46,70 @@ return array(
   */
   'css' => 'css/style.css?v=0.1',
 
+
+  /*
+  |--------------------------------------------------------------------------
+  | jQuery
+  |--------------------------------------------------------------------------
+  |
+  | jQuery configuration here:
+  |
+  |   Choose the version of jQuery you want to work with.
+  |   You should always provide a fallback in case something 
+  |   doesn't work with the remote server.
+  |
+  | Modelo has some defaults for you :)
+  |
+  | You don't have to use jQuery, though.
+  | Simply set 'jquery_on' to false if you don't care about jQuery.
+  |
+  */
+
+  'jquery_on'       => true,
+  'jquery_version'  => '1.8.3',
+  'jquery_fallback' => 'js/libs/jquery.min.js',
+
+
+  /*
+  |--------------------------------------------------------------------------
+  | Javascript files
+  |--------------------------------------------------------------------------
+  |
+  | Define the path to your other javascript files here.
+  | Because of the way Laravel works, the 'scripts' variable has to be
+  | an associative array.
+  |
+  | Add them as if you were starting from the root of your site.
+  | Here is an example:
+  |
+  |   'scripts' => array(
+  |     'script'  => 'js/script.js',
+  |     'plugins' => 'js/plugins.js'
+  |   ),
+  |
+  */
+  
+  'scripts' => array(
+    
+  ),
+
+
+  /*
+  |--------------------------------------------------------------------------
+  | Google Analytics
+  |--------------------------------------------------------------------------
+  |
+  | Set your google analytics site ID here.
+  |
+  | This is an asynchronous google analytics snippet, as found
+  | on HTML5 Boilerplate.
+  |
+  | Read more about it here:
+  | mathiasbynens.be/notes/async-analytics-snippet
+  | 
+  */
+
+  'analytics_on' => false,
+  'analytics_id' => 'UA-XXXXX-X',
+
 );
