1d0584ebbe7c81af29e67bccd82f492e31613601
Carlos Castillo <carlos@koalabs.co>
2013-02-28 22:39:42 -0500

    Main head content

---
+++ config/modelo.php
@@ -0,0 +1,49 @@
+<?php
+
+return array(
+
+  /*
+  |--------------------------------------------------------------------------
+  | Language
+  |-------------------------------------------------------------------------- 
+  |
+  | Set the language of your site here.
+  | And remember, spanish is 'es', not 'sp'.
+  |
+  | Read more about the lang attribute:
+  | http://tlt.its.psu.edu/suggestions/international/web/tips/langtag.html#why
+  |
+  */
+
+  'language' => 'en',
+
+
+  /*
+  |--------------------------------------------------------------------------
+  | The author
+  |--------------------------------------------------------------------------
+  |
+  | This is used for filling out the author meta tag.
+  | Just write how you would like to be named here.
+  |
+  */
+
+  'author' => 'The Koalabs Team',
+
+
+  /*
+  |--------------------------------------------------------------------------
+  | CSS Main file
+  |--------------------------------------------------------------------------
+  |
+  | Path to your main css file here.
+  | Because you ARE concatenating and minifying your css files, aren't you?
+  | Or at least using sass?
+  | 
+  | If not, you will need to go to the 'head.blade.php' file and add the css
+  | files you are using in there.
+  | 
+  */
+  'css' => 'css/style.css?v=0.1',
+
+);
